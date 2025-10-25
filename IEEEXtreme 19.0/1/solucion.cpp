33ee7d92a478fb11ad6916c3a4ddd5f1ba4e86db
emeorg <michelle.ramirez@mail.udp.cl>
2025-10-24 21:20:40 -0300

    feat: actualizar archivos de entrada/salida y mejorar solución en C++

--- IEEEXtreme 19.0/1/solucion.cpp
+++ IEEEXtreme 19.0/1/solucion.cpp
@@ -1,18 +1,67 @@
-#include <iostream>
-#include <unistd.h>
-
+#include <bits/stdc++.h>
 using namespace std;
+#define FOR(a, c) for (int(a) = 0; (a) < (c); (a)++)
+typedef long long int ll;
 
 int main()
 {
-    int a;
-    cin >> a;
-    // esperar 5     segundos
-    sleep(4);
-    for (int i = 0; i < a; i++)
-    {
-        cout << "Hello, World!" << endl;
-    }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
+    ll N, Q;
+    cin >> N >> Q;
+    ll arr[N];
+
+    FOR(i, N)
+    cin >> arr[i];
+
+    FOR(i, Q)
+    {
+        ll query;
+        cin >> query;
+        // Binary Search
+        ll left = 0, right = N - 1;
+        ll ansfirst = -1, anslast = -1;
+
+        while (left <= right)
+        {
+            ll mid = left + (right - left) / 2;
+            if (arr[mid] == query)
+            {
+                ansfirst = mid + 1;
+                right = mid - 1;
+            }
+            else if (arr[mid] < query)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        left = 0;
+        right = N - 1;
+        while (left <= right)
+        {
+            ll mid = left + (right - left) / 2;
+            if (arr[mid] == query)
+            {
+                anslast = mid + 1;
+                left = mid + 1;
+            }
+            else if (arr[mid] < query)
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        cout << ansfirst << " " << anslast << "\n";
+    }
     return 0;
 }
